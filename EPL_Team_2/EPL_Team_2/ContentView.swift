f803ba303813c15285068c050003d49214ce0f4e
Jayfunf <simh3077@gmail.com>
2021-10-18 14:37:00 +0900

    init: first_commit

---
+++ EPL_Team_2/EPL_Team_2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EPL_Team_2
+//
+//  Created by Minhyun Cho on 2021/10/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
