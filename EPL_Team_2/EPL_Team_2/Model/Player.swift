d02aac999abb91d616d2b6da4393dd8446cdc592
Jayfunf <simh3077@gmail.com>
2021-11-15 21:12:28 +0900

    modify: struct -> class add: initializer

--- EPL_Team_2/EPL_Team_2/Model/Player.swift
+++ EPL_Team_2/EPL_Team_2/Model/Player.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct Player: Identifiable{
+class Player: Identifiable, ObservableObject {
     var id: Int
     var club_name: String
     var uniform_number: String
@@ -20,4 +20,19 @@ struct Player: Identifiable{
     var weight: String
     var player_img_url: String?
     var player_detail_url: String?
+    
+    init(id: Int, club_name: String, uniform_number: String, name: String, position: String, country: String, date_of_birth: String, age: Int, height: String, weight: String, player_img_url: String?, player_detail_url: String?) {
+        self.id = id
+        self.club_name = club_name
+        self.uniform_number = uniform_number
+        self.name = name
+        self.position = position
+        self.country = country
+        self.date_of_birth = date_of_birth
+        self.age = age
+        self.height = height
+        self.weight = weight
+        self.player_img_url = player_img_url
+        self.player_detail_url = player_detail_url
+    }
 }
