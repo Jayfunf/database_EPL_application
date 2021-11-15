a4f5fdfdd9717e6235b5808069c6fc07670cffbb
Jayfunf <simh3077@gmail.com>
2021-11-15 21:12:01 +0900

    modify: struct -> class add: initializer

--- EPL_Team_2/EPL_Team_2/Model/Club.swift
+++ EPL_Team_2/EPL_Team_2/Model/Club.swift
@@ -7,12 +7,12 @@
 
 import Foundation
 
-struct Club: Identifiable{
+class Club: Identifiable, ObservableObject {
     var id: Int
     var club_name: String
     var stadium: String
-    var badge_images_url: String
-    var club_url: String
+    var badge_images_url: String?
+    var club_url: String?
     var position: String
     var played: String //Time?
     var won: String
@@ -23,4 +23,22 @@ struct Club: Identifiable{
     var gd: String
     var points: String
     var form: String
+    
+    init(id: Int, club_name: String, stadium: String, badge_images_url: String?, club_url: String?, position: String, played: String, won: String, drawn: String, lost: String, gf: String, ga: String, gd: String, points: String, form: String) {
+        self.id = id
+        self.club_name = club_name
+        self.stadium = stadium
+        self.badge_images_url = badge_images_url
+        self.club_url = club_url
+        self.position = position
+        self.played = played
+        self.won = won
+        self.drawn = drawn
+        self.lost = lost
+        self.gf = gf
+        self.ga = ga
+        self.gd = gd
+        self.points = points
+        self.form = form
+    }
 }
